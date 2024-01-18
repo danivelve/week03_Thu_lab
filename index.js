8b2536bc1b8b2148fb1b3a1c6edcaa234a73cd98
danivelve <daniel.d.appiah@outlook.com>
2024-01-18 16:34:46 -0500

    STring Edit

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log("Git Example")
+
+var str = "Hello World"
+var s = new String("Hello World")
+var s1 = new String("Hello World")
+var s2 = new String("Hello World")
+if(str ===s){
+    console.log("Equal")
+}else{
+    console.log("not Equal")
+}
